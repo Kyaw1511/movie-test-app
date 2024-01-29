693f970a6d16ce0da80ab9deb7f8c43ec2142d60
Kyaw1511 <kyawminthein151193@gmail.com>
2024-01-29 22:59:26 +0630

    adding navbar section

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,35 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <!-- Navbar section -->
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Movie-App</a>
+      <button 
+        class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Features
+            </a>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+</nav>
+
+</template>
+
+<!-- <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
@@ -18,9 +47,9 @@ import HelloWorld from './components/HelloWorld.vue'
   </header>
 
   <RouterView />
-</template>
+</template> -->
 
-<style scoped>
+<!-- <style scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -82,4 +111,4 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
-</style>
+</style> -->
