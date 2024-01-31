305b239b8b4ae821f460fdcdb05844a4cf679cca
Kyaw1511 <kyawminthein151193@gmail.com>
2024-01-31 21:41:22 +0630

    test9

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import MovieView from '../App.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createRouterHome(  import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
