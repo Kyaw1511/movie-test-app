b3aa5b292525731a2d41add651675f1deef97667
Kyaw1511 <kyawminthein151193@gmail.com>
2024-01-31 21:24:57 +0630

    test 7

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
+
+
 <template>
-
-  <!-- testing 1 -->
-  <h1> Hello vue.js </h1>
-  <h2>Movie</h2>
-  
-
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Movie</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+        
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
+
 
 <script setup>
 </script>
