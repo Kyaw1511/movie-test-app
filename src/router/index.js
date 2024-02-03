02a7f7a005208598f93c098c15c6b8b3d5e874ca
Kyaw1511 <kyawminthein151193@gmail.com>
2024-02-03 19:57:40 +0630

    change index file part 2

--- src/router/index.js
+++ src/router/index.js
@@ -23,37 +23,37 @@
 // export default router
 
 
-// import { createApp } from "vue";
+import { createApp } from "vue";
 
-// import App from './App.vue'
+import App from './App.vue'
 
-// import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
-// // step 1
-// const Home = () => import('./pages/Home.vue')
-// const About = () => import('./pages/About.vue')
+// step 1
+const Home = () => import('./pages/Home.vue')
+const About = () => import('./pages/About.vue')
 
-// // step 2
-// const routes = [
-//     {
-//         path: '/movie-test-app', 
-//         component: Home
-//     },
-//     {
-//         path: '/movie-test-app/about',
-//         component: About
-//     }
-// ]
+// step 2
+const routes = [
+    {
+        path: '/movie-test-app', 
+        component: Home
+    },
+    {
+        path: '/movie-test-app/about',
+        component: About
+    }
+]
 
-// // step 3
-// const router = createRouter({
-//     history: createWebHistory(),
-//     routes,
-// })
+// step 3
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
 
 
-// const app = createApp(App)
-// // step 4
-// app.use(router)
-// app.mount('#app')
+const app = createApp(App)
+// step 4
+app.use(router)
+app.mount('#app')
 
